234e101db30ce4fcfd349371ae4458f31db423d1
Kier Davis <kierdavis@gmail.com>
2020-01-10 23:07:03 +0000

    Allow helm-deploy to uninstall as well as install

--- tools/helm-deploy.sh
+++ tools/helm-deploy.sh
@@ -9,6 +9,12 @@ set -o errexit -o nounset -o pipefail
 #   chart_name: name of the helm chart within its repository
 #   values: path to yaml file containing configuration values for the release
 
+if [ $# -lt 1 ]; then
+  echo >&2 "usage: $0 up|down"
+  exit 2
+fi
+operation="$1"
+
 if [ -z "${release_name:-}" ]; then
   echo >&2 "release_name must be set"
   exit 1
@@ -18,27 +24,38 @@ if [ -n "${release_namespace:-}" ]; then
 else
   ns_option=""
 fi
-if [ -z "${chart_repo:-}" ]; then
-  echo >&2 "chart_repo must be set"
-  exit 1
-fi
-if [ -z "${chart_name:-}" ]; then
-  echo >&2 "chart_name must be set"
-  exit 1
-fi
-if [ -n "${values:-}" ]; then
-  values_option="--values=$values"
-else
-  values_option=""
-fi
 
-if helm status $ns_option "$release_name" >/dev/null 2>&1; then
-  # release already exists
-  operation=upgrade
-else
-  # release doesn't already exist
-  operation=install
-fi
+if [ "$operation" = "up" ]; then
+  if [ -z "${chart_repo:-}" ]; then
+    echo >&2 "chart_repo must be set"
+    exit 1
+  fi
+  if [ -z "${chart_name:-}" ]; then
+    echo >&2 "chart_name must be set"
+    exit 1
+  fi
+  if [ -n "${values:-}" ]; then
+    values_option="--values=$values"
+  else
+    values_option=""
+  fi
 
-set -x
-exec helm $operation $ns_option $values_option --repo="$chart_repo" "$release_name" "$chart_name"
+  if helm status $ns_option "$release_name" >/dev/null 2>&1; then
+    # release already exists
+    helm_operation=upgrade
+  else
+    # release doesn't already exist
+    helm_operation=install
+  fi
+
+  set -x
+  exec helm $helm_operation $ns_option $values_option --repo="$chart_repo" "$release_name" "$chart_name"
+
+elif [ "$operation" = "down" ]; then
+  set -x
+  exec helm uninstall $ns_option "$release_name"
+
+else
+  echo >&2 "invalid operation: $operation"
+  exit 2
+fi
