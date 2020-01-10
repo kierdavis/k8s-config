e6b0c8d19919c413f5d92a3adfe55d31f2890c24
Kier Davis <kierdavis@gmail.com>
2020-01-10 23:51:43 +0000

    apply-all.sh: also deploy helm apps as well as raw yaml

--- apply-all.sh
+++ apply-all.sh
@@ -3,3 +3,7 @@ for file in $(find manifests -name '*.yaml'); do
   echo "[$file]" >&2
   kubectl apply -f $file
 done
+for file in $(find manifests -name '*.deploy'); do
+  echo "[$file]" >&2
+  $file up
+done
